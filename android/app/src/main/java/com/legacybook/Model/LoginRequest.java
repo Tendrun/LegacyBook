ee5da64006056686db8ba8c4a324fe0eefc4322e
Tendrun <mikisz2002@gmail.com>
2025-04-25 16:28:12 +0200

    Folder fix LegacyKeep

--- android/app/src/main/java/com/legacybook/Model/LoginRequest.java
+++
@@ -1,6 +0,0 @@
-package com.legacybook.Model;
-
-public class LoginRequest {
-    private String email, password;
-    public LoginRequest(String email, String password) { this.email = email; this.password = password; }
-}
--- android/app/src/main/java/com/legacybook/Model/RegisterRequest.java
+++
@@ -1,6 +0,0 @@
-package com.legacybook.Model;
-
-public class RegisterRequest {
-    private String email, password;
-    public RegisterRequest(String email, String password) { this.email = email; this.password = password; }
-}
